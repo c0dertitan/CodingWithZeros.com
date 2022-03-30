bfd70a14c9058d3ab86ca0afb158d59d7febf7af
OctoGit <98435530+c0dertitan@users.noreply.github.com>
2022-03-30 07:49:19 -0500

    Learn about echo with zeros!

---
+++ Zero'sSaysEcho.sh
@@ -0,0 +1,25 @@
+#  /----------\    Hello, I am a Robot named: Zeros
+# /            \   I am trying to find my pet dog in this code his name 
+#/     O   O    \  His name is: Ones
+#|       ^      |  Anyways, good luck!
+#|              |
+#|    -------   |
+#|              |
+#|______________|
+
+
+echo "Hey it is me Zeros if you see the word 'echo' that is the code of text running to the output also known as Terminal without any commands in the way!"
+echo "I dropped my leash, and my dog ran off"
+echo "Hello, World!"
+echo "Beep Boop, Learn More About Coding at c0dertitan.github.io/CodingWithZeros.com/"
+echo "Bark, Bark! My name is Ones"
+#  /----------\    Yay! You found my dog! Thank you so much!
+# /            \   
+#/     O   O    \  
+#|       ^      |  
+#|              |
+#|    -------   |
+#|              |
+#|______________|
+
+echo "To learn MORE about coding with shell scripts go to c0dertitan.github.io/CodingWithZeros.com/"
